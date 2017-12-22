251c486827302d34774ff1dc6b74c83a36ad905c
matthewholey <matthewbholey@gmail.com>
2017-12-22 11:25:17 -0800

    delete + put routes functional, buttons connected, up to date

--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
+	$(".delete-link").click(function(e){
+		e.preventDefault();
 
+		$.ajax({
+			method: "DELETE",
+			url: $(this).attr("href")
+		}).done(function(response){
+			console.log("promise reached");
+			window.location.href = "/teams";
+		});
+	});
+
+	$(".edit-form").submit(function(e){
+		e.preventDefault();
+
+		$.ajax({
+			method: "PUT",
+			url: $(this).attr("action"),
+			data: {
+				name: $("#new-name").val(),
+				members: $("#members").val()
+			}
+		}).done(function(response){
+			console.log("promise successful");
+			window.location.href = "/teams";
+		});
+	});
 });
+
